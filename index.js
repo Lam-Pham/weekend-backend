b7dd93f2433366526a957465953301e625b0b8fe
Lam-Pham <lpham127@gmail.com>
2021-05-23 20:11:37 -0700

    set up server and sample data

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+let spots = [
+    {
+        id: 1,
+        activity: "Go surfing",
+        location: "Seal Beach",
+        date: "2019-05-30T17:30:31.098Z"
+      },
+      {
+        id: 2,
+        activity: "Get food and drinks",
+        location: "2nd and PCH",
+        date: "2019-05-30T18:39:34.091Z"
+      },
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/spots', (request, response) => {
+  response.json(spots)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
