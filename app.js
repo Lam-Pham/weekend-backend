77d2cadaf2375287319d84cb8be8a042f337e784
Lam-Pham <lpham127@gmail.com>
2021-05-26 19:08:24 -0700

    exported db connection to app module

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const spotsRouter = require('./controllers/spots')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(result => {
+        logger.info('connected to MongoDB!')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/spots', spotsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- models/spot.js
+++ models/spot.js
@@ -1,15 +1,5 @@
 const mongoose = require('mongoose')
 
-const url = config.MONGODB_URI
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(result => {
-        logger.info('connected to MongoDB!')
-    })
-    .catch((error) => {
-        logger.error('error connecting to MongoDB:', error.message)
-    })
-
 const spotSchema = new mongoose.Schema({
     activity: {
       type: String,
