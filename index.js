a026d00e880a91915f3a2240fec88b39d8758d90
Lam-Pham <lpham127@gmail.com>
2021-05-26 19:08:51 -0700

    removed route handlers from index.js

--- index.js
+++ index.js
@@ -21,58 +21,7 @@ app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
 
-// ROUTES
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/spots', (request, response) => {
-    Spot.find({}).then(spots => {
-        response.json(spots)
-    })
-})
-
-app.get('/api/spots/:id', (request, response) => {
-    Spot.findById(request.params.id)
-        .then(spot => {
-            if (spot) {
-                response.json(spot)
-            } else {
-                response.status(404).end()
-            } 
-        })
-        .catch(error => next(error))            // passing off to error handling middleware
-})
-
-app.post('/api/spots', (request, response) => {
-    const body = request.body
-    if ((body.activity || body.location) === undefined) {
-        return response.status(400).json({ 
-          error: 'content missing' 
-        })
-    }
-
-    const spot = new Spot({
-        activity: body.activity,
-        location: body.location,
-        date: new Date(),
-    })
-
-    spot.save()
-        .then(savedSpot => {
-            response.json(savedSpot)
-        })
-        .catch(error => next(error))
-})
-
-app.delete('/api/spots/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
-})
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
