21c1c622d279de84374bf360fbef061f0fd92c35
Lam-Pham <lpham127@gmail.com>
2021-05-28 10:48:06 -0400

    installed jest; added async/await test

--- tests/spot_api.test.js
+++ tests/spot_api.test.js
@@ -6,10 +6,16 @@ const app = require('../app')
 const api = supertest(app)
 
 test('spots are returned as json', async () => {
-  await api
-    .get('/api/spots')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+    await api
+        .get('/api/spots')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('there are at least two spots', async () => {
+    const response = await api.get('/api/spots')
+  
+    expect(response.body).not.toHaveLength(0 || 1)
 })
 
 afterAll(() => {
